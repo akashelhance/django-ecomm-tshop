2700ab4f27f217347bd4adca9eea1412e344bbce
akash elhance <akashelhance@gmail.com>
2021-04-24 00:49:16 +0530

    Auth done

--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 
-from store.views import home, cart, login,signup,orders
+from store.views import home, cart, login,signup,orders,signout
 
 urlpatterns = [
    path('' , home , name='homepage'),
    path('cart/' , cart , name='cart'),
    path('login/' , login , name='login'), 
    path('signup/' , signup , name='signup'),
-   path('orders/' , orders , name='orders'),  
-
-   
-      
+   path('orders/' , orders , name='orders'),
+   path('logout/' , signout , name='logout'),   
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,17 +1,70 @@
 from django.shortcuts import render
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from .forms.authforms import CustomerCreationForm, CustomerAuthForm
+from django.contrib.auth import authenticate, login as LoginDjango, logout
+
 # Create your views here.
 def home(request):
+    print(request.user)
     return render(request, template_name='store/home.html')
 
 def cart(request):
     return render(request, template_name='store/cart.html')
 
 def login(request):
-    return render(request, template_name='store/login.html')
+    if request.method == 'POST':
+        form=CustomerAuthForm(data=request.POST)
+        if form.is_valid():
+            username= form.cleaned_data.get('username')
+            password=form.cleaned_data.get('password')
+            user=authenticate(username=username,password=password)
+            if user:
+                LoginDjango(request, user)
+                return redirect('homepage')
+            else:
+                pass
+
+        else:
+            context ={
+            "form":form
+        }
+        return render(request, template_name='store/login.html' , context=context)
+
+        
+    else:
+        form= CustomerAuthForm()
+        context ={
+            "form":form
+        }
+        return render(request, template_name='store/login.html' , context=context)
 
 def signup(request):
-    return render(request, template_name='store/signup.html')
+    if request.method=='POST':
+        form=CustomerCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            user.email=user.username
+            user.save()
+            print(user)
+            return render(request, template_name='store/login.html')
+        else:
+            context ={
+            "form":form
+        }
+            return render(request, template_name='store/signup.html', context=context)
+
+    else:
+        form=CustomerCreationForm()
+        context ={
+            "form":form
+        }
+        return render(request, template_name='store/signup.html', context=context)
 
 def orders(request):
     return render(request, template_name='store/orders.html')
+
+def signout(request):
+    # request.session.clear()
+    logout(request)
+    return render(request, template_name='store/home.html')
